1efbd055de423eb21c1b69e83710a5a4a3a7151e
masoume mozaffari <masoumemozaffari23@gmail.com>
2026-07-22 15:19:25 +0330

    Add menu item post type and menu section taxonomy

--- simple-restaurant-menu.php
+++ simple-restaurant-menu.php
@@ -5,3 +5,28 @@
  * Version: 1.0
  * Author: Masoume Mozaffari
  */
+function register_menu_item_post_type() {
+    register_post_type('menu_item', array(
+        'labels' => array(
+            'name' => 'آیتم‌های منو',
+            'singular_name' => 'آیتم منو',
+        ),
+        'public' => true,
+        'has_archive' => true,
+        'menu_icon' => 'dashicons-carrot',
+        'supports' => array('title', 'editor', 'thumbnail'),
+    ));
+}
+add_action('init', 'register_menu_item_post_type');
+function register_menu_section_taxonomy() {
+    register_taxonomy('menu_section', 'menu_item', array(
+        'labels' => array(
+            'name' => 'بخش‌های منو',
+            'singular_name' => 'بخش منو',
+        ),
+        'hierarchical' => true,
+        'public' => true,
+        'show_ui' => true,
+    ));
+}
+add_action('init', 'register_menu_section_taxonomy');
