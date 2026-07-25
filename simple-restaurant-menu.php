0dcf10534833818a34874fe92fed888aa3058ff0
masoume mozaffari <masoumemozaffari23@gmail.com>
2026-07-25 10:19:57 +0330

    Add price and dietary badge fields with Carbon Fields

--- simple-restaurant-menu.php
+++ simple-restaurant-menu.php
@@ -1,32 +1,63 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Carbon_Fields\Carbon_Fields;
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
 /**
  * Plugin Name: Simple Restaurant Menu
  * Description: افزونه‌ای برای نمایش منوی رستوران
  * Version: 1.0
  * Author: Masoume Mozaffari
  */
+
 function register_menu_item_post_type() {
     register_post_type('menu_item', array(
         'labels' => array(
             'name' => 'آیتم‌های منو',
             'singular_name' => 'آیتم منو',
         ),
-        'public' => true,
+        'public'      => true,
         'has_archive' => true,
-        'menu_icon' => 'dashicons-carrot',
-        'supports' => array('title', 'editor', 'thumbnail'),
+        'menu_icon'   => 'dashicons-carrot',
+        'supports'    => array('title', 'editor', 'thumbnail'),
     ));
 }
 add_action('init', 'register_menu_item_post_type');
+
 function register_menu_section_taxonomy() {
     register_taxonomy('menu_section', 'menu_item', array(
         'labels' => array(
-            'name' => 'بخش‌های منو',
+            'name'          => 'بخش‌های منو',
             'singular_name' => 'بخش منو',
         ),
         'hierarchical' => true,
-        'public' => true,
-        'show_ui' => true,
+        'public'       => true,
+        'show_ui'      => true,
     ));
 }
 add_action('init', 'register_menu_section_taxonomy');
+
+function load_carbon_fields() {
+    Carbon_Fields::boot();
+}
+add_action('after_setup_theme', 'load_carbon_fields');
+
+function create_menu_item_fields() {
+    Container::make('post_meta', 'جزئیات آیتم منو')
+        ->where('post_type', '=', 'menu_item')
+        ->add_fields(array(
+            Field::make('text', 'menu_item_price', 'قیمت (تومان)'),
+
+            Field::make('set', 'menu_item_badges', 'نشان‌های رژیمی')
+                ->add_options(array(
+                    'vegetarian' => 'گیاهی',
+                    'nuts'        => 'حاوی آجیل',
+                    'dairy'       => 'حاوی لبنیات',
+                    'spicy'       => 'تند',
+                    'glutenfree'  => 'بدون گلوتن',
+                )),
+        ));
+}
+add_action('carbon_fields_register_fields', 'create_menu_item_fields');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1efbd055de423eb21c1b69e83710a5a4a3a7151e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1efbd055de423eb21c1b69e83710a5a4a3a7151e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'htmlburger/carbon-fields' => array(
+            'pretty_version' => 'v3.6.9',
+            'version' => '3.6.9.0',
+            'reference' => 'f82e80e3e3469d6e86cc17a8950b918ad448a059',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../htmlburger/carbon-fields',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
